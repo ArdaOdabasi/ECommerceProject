90ad5957c0b21d341d07265e916ab1d667c3e3fa
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-10-14 20:45:35 +0300

    Third Commit
    The project was created as an ASP.NET Core Web App (Model-View-Controller) project.
    
    The Identity Area is created automatically when the project is created.
    
    I created two more areas named Customer and Admin.

--- ECommerceProject/obj/Debug/net5.0/Razor/Views/Shared/Error.cshtml.g.cs
+++ ECommerceProject/obj/Debug/net5.0/Razor/Views/Shared/Error.cshtml.g.cs
@@ -26,8 +26,15 @@ using ECommerceProject.Models;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 3 "C:\Users\pi\source\repos\ECommerceProject\ECommerceProject\Views\_ViewImports.cshtml"
+using ECommerceProject.Areas.Customer;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d6a5625cc8fb4476f348b0fe9041c550465d8bf9", @"/Views/Shared/Error.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"2dee84abad4093d560eef049945b805a06b417e2", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"a2d2966b85a49133772659679db136a6558eaab2", @"/Views/_ViewImports.cshtml")]
     public class Views_Shared_Error : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<ErrorViewModel>
     {
         #pragma warning disable 1998
