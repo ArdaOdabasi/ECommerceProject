1b0d573a3b94574f49dd334bbd5393cdbe45099e
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-10-25 00:15:17 +0300

    Fifth Commit
    I created a model named Product.
    
    I added a migration to add a table called products to the database.
    
    I created a controller named Product Controller.
    
    I added products to the menu to manage products from the Admin Panel.
    
    I added DataTable to list products.
    
    I created product index page, add product page, edit product page, product details page and delete product page.

---
+++ ECommerceProject/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECommerceProject.Data;
+using ECommerceProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ECommerceProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Admin/Product
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Products.Include(p => p.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Product/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Product/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var Files = HttpContext.Request.Form.Files;
+                if (Files.Count > 0)
+                {
+                    string FileName = Guid.NewGuid().ToString();
+                    var Uploads = Path.Combine(_webHostEnvironment.WebRootPath, @"images\Products");
+                    var Extended = Path.GetExtension(Files[0].FileName);
+
+                    if (product.Image != null)
+                    {
+                        var ImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.Image.TrimStart('\\'));
+                        if (System.IO.File.Exists(ImagePath))
+                        {
+                            System.IO.File.Delete(ImagePath);
+                        }
+                    }
+
+                    using (var FilesStreams = new FileStream(Path.Combine(Uploads, FileName + Extended), FileMode.Create))
+                    {
+                        Files[0].CopyTo(FilesStreams);
+                    }
+
+                    product.Image = @"\images\Products\" + FileName + Extended;
+                }
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Product/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Admin/Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Product product)
+        {           
+            if (ModelState.IsValid)
+            {
+                var Files = HttpContext.Request.Form.Files;
+                if (Files.Count > 0)
+                {
+                    string FileName = Guid.NewGuid().ToString();
+                    var Uploads = Path.Combine(_webHostEnvironment.WebRootPath, @"images\Products");
+                    var Extended = Path.GetExtension(Files[0].FileName);
+
+                    if (product.Image != null)
+                    {
+                        var ImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.Image.TrimStart('\\'));
+                        if (System.IO.File.Exists(ImagePath))
+                        {
+                            System.IO.File.Delete(ImagePath);
+                        }
+                    }
+
+                    using (var FilesStreams = new FileStream(Path.Combine(Uploads, FileName + Extended), FileMode.Create))
+                    {
+                        Files[0].CopyTo(FilesStreams);
+                    }
+
+                    product.Image = @"\images\Products\" + FileName + Extended;
+                }
+                
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(product);
+        }
+
+        // GET: Admin/Product/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            var ImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.Image.TrimStart('\\'));
+            if (System.IO.File.Exists(ImagePath))
+            {
+                System.IO.File.Delete(ImagePath);
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
--- ECommerceProject/Migrations/20221018134311_AddProduct.Designer.cs
+++ ECommerceProject/Migrations/20221018134311_AddProduct.Designer.cs
@@ -4,14 +4,16 @@ using ECommerceProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ECommerceProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221018134311_AddProduct")]
+    partial class AddProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
